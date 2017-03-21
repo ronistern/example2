bbdbdffc2237bf40ca7ffd921ae1f601703fabe5
roni.stern <roni.stern@gmail.com>
2017-03-21 12:58:05 +0200

    Added a new nice set of nice classes

---
+++ GitExamples/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Hello world!");
+            Console.WriteLine("Hello small class!");
+            Console.WriteLine("Hello too big class!");
+
+            int i = 12;
+        }
+
+        void f()
+        {
+            string h = "Merge is cool";
+            string bb = "branchdddeee!";
+        }
+
+        void g()
+        {
+            string ddd = "dd";
+        }
+
+        void h()
+        {
+            string dddd = "d";
+        }
+    }
+}
